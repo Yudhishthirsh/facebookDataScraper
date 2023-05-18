ec01dd43099d94e61b3a3db42e86e7c3ca82c648
Yudhishthirsh <130043970+Yudhishthirsh@users.noreply.github.com>
2023-05-18 16:33:26 +0530

    Update parameter.py

--- facebookLikesScraper/parameter.py
+++ facebookLikesScraper/parameter.py
@@ -5,5 +5,15 @@ Facebook_password = 'Password'
 URL= "https://www.facebook.com/groups/1225966920763001"
 #Enter the relative path of your chromedriver
 relative_path = "./chromedriver.exe"
+
+# Define the Random file name
+length = 5
+# Generate a random string
+random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+# Get the current timestamp
+timestamp = time.strftime("%Y%m%d%H%M%S")
+fileName = timestamp + random_string + '.csv' #this the generated filename
+print("Your output file is: ",fileName )
+
 #Enter number of scrolls you want.
 noOfScrolls = 100
