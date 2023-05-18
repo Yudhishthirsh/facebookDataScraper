370e4a36ee963bfeaf0b2fc334c333fb3c44cfbe
Yudhishthirsh <130043970+Yudhishthirsh@users.noreply.github.com>
2023-05-18 16:35:20 +0530

    Update parameter.py

--- facebookLikesScraper/parameter.py
+++ facebookLikesScraper/parameter.py
@@ -4,7 +4,7 @@ Facebook_password = 'Password'
 #url from which need to scrape data
 URL= "https://www.facebook.com/groups/1225966920763001"
 #Enter the relative path of your chromedriver
-relative_path = "./chromedriver.exe"
+chromeDriverPath = "./chromedriver.exe"
 
 # Define the Random file name
 length = 5
